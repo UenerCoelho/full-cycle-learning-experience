8207a1f5a8606357f3903b347263100cf402c077
Uener Coelho <uenercoelho2@hotmail.com>
2023-04-26 21:53:09 -0400

    Chat.go created

--- internal/domain/entity/chat.go
+++ internal/domain/entity/chat.go
@@ -1,1 +1,16 @@
 package entity
+
+type ChatConfig struct {
+	Model *Model
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Message              []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
